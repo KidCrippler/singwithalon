c1d1a19d39e16d36b9d0160f5c0f03c074d89cd0
Alon Cohen <alonc@ziprecruiter.com>
2025-08-16 04:26:45 +0300

    Removing irrelevant registration code

--- js/script.js
+++ js/script.js
@@ -973,15 +973,3 @@ window.addEventListener('resize', () => {
     }
 });
 
-// Service Worker registration for PWA capabilities (optional)
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(registration => {
-                console.log('SW registered: ', registration);
-            })
-            .catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
-    });
-}
